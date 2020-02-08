c8c1021cf3c73e84159fb987e4fc5781ac69a209
Ethan Anders <ethananders97@gmail.com>
2020-02-08 15:51:57 -0800

    Fixed new replay path.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const { app } = require ('electron');
 
 module.exports = {
   watch: [
-    `${app.getPath ('home')}/Documents/Warcraft III/Replay/**/*.w3g`,
-    `!${app.getPath ('home')}/Documents/Warcraft III/Replay/**/TempReplay.w3g`
+    `${app.getPath ('home')}/Documents/Warcraft III/BattleNet/**/*.w3g`,
+    `!${app.getPath ('home')}/Documents/Warcraft III/BattleNet/**/TempReplay.w3g`
   ]
 };
